8283fdab630898e3b6b6de986e7a4b63626d8cf7
DoseiBrayin <ardilabrayan68@gmail.com>
2022-12-29 20:40:48 -0500

    (config) add a staments of petition class

--- src/main/java/com/example/ApiRest/models/petition.java
+++ src/main/java/com/example/ApiRest/models/petition.java
@@ -1,4 +1,17 @@
 package com.example.ApiRest.models;
 
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
 public class petition {
+    @Id
+    private String idpetition;
+    private String description;
+    private String state;
+    private Date create_stamp;
+    private String plataform;
+
+    private client client;
+
 }
