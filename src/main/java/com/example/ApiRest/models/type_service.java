dd2a37898692be81f30a8f0fe0553ac834dd6bcc
DoseiBrayin <ardilabrayan68@gmail.com>
2022-12-29 20:43:42 -0500

    (config) add a staments of type service class

--- src/main/java/com/example/ApiRest/models/type_service.java
+++ src/main/java/com/example/ApiRest/models/type_service.java
@@ -1,4 +1,10 @@
 package com.example.ApiRest.models;
 
+import jakarta.persistence.Id;
+
 public class type_service {
+    @Id
+    private String idtype_service;
+    private String service_type;
+    private service service;
 }
