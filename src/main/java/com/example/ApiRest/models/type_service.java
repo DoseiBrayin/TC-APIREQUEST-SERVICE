cb0b35e9a702fce35da49e8e198a5a8fed1cbb28
DoseiBrayin <ardilabrayan68@gmail.com>
2022-12-29 20:47:22 -0500

    (config) add a config table annotation in type service class

--- src/main/java/com/example/ApiRest/models/type_service.java
+++ src/main/java/com/example/ApiRest/models/type_service.java
@@ -1,7 +1,11 @@
 package com.example.ApiRest.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "type_service")
 public class type_service {
     @Id
     private String idtype_service;
