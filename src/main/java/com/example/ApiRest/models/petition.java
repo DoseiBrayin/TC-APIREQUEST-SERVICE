ee8aa111db269f9c59f2276f6837904310908324
DoseiBrayin <ardilabrayan68@gmail.com>
2022-12-29 20:46:42 -0500

    (config) add a config table annotation in petition class

--- src/main/java/com/example/ApiRest/models/petition.java
+++ src/main/java/com/example/ApiRest/models/petition.java
@@ -1,9 +1,12 @@
 package com.example.ApiRest.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.util.Date;
-
+@Entity
+@Table(name = "petition")
 public class petition {
     @Id
     private String idpetition;
