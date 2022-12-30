80c18a89bac7588a3ec486341d0341222f410f71
DoseiBrayin <ardilabrayan68@gmail.com>
2022-12-29 20:42:19 -0500

    (config) add a staments of service class

--- src/main/java/com/example/ApiRest/models/service.java
+++ src/main/java/com/example/ApiRest/models/service.java
@@ -1,4 +1,11 @@
 package com.example.ApiRest.models;
 
+import jakarta.persistence.Id;
+
 public class service {
+    @Id
+    private String idservice;
+    private String name_service;
+
+    private  client client;
 }
