7bb4b3caeb80321ea749ea5ad61c78dce852c4cf
DoseiBrayin <ardilabrayan68@gmail.com>
2023-01-13 17:52:07 -0500

    (add) is incorporated type_service funtion, is implemented 'type service' controller and its router

--- src/main/java/com/example/ApiRest/controllers/type_service_controller.java
+++ src/main/java/com/example/ApiRest/controllers/type_service_controller.java
@@ -1,4 +1,24 @@
 package com.example.ApiRest.controllers;
 
+import com.example.ApiRest.models.type_service;
+import com.example.ApiRest.services.type_service_service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/type_service")
 public class type_service_controller {
+    @Autowired
+    private type_service_service type_service_service;
+
+    @GetMapping("/")
+    private ResponseEntity<List<type_service>> getAllType(){
+        if(type_service_service.findAll().isEmpty()) return ResponseEntity.notFound().build();
+        return  ResponseEntity.ok(type_service_service.findAll());
+    }
 }
